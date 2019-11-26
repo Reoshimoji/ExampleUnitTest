32ce7765aa74a26d539969c03126afe8c8bca87c
Shimoji Reo <e195758@ie.u-ryukyu.ac.jp>
2019-11-26 11:56:32 +0900

    step2

--- src/main/jp/ac/uryukyu/ie/e195758/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195758/Enemy.java
@@ -1,48 +1,8 @@
 package jp.ac.uryukyu.ie.e195758;
 
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
-
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(dead != true) {
-            int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
+public class Enemy extends LivingThing {
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
     }
 
     /**
